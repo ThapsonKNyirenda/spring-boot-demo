11b4636f6084afac4ed74c6943b89275e972a345
Thapson <thapsonknyirenda@gmail.com>
2024-12-25 23:14:01 +0200

    Initial commit

---
+++ src/main/java/com/thapson/Main.java
@@ -0,0 +1,33 @@
+package com.thapson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse(
+                "Hello Thapson",
+                List.of("Java", "PHP", "Javascript"),
+                new Person("Thapson", "Nyirenda")
+        );
+        return response;
+    }
+
+    record Person(String name, String surname){};
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){};
+}
