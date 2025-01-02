c9e51792a41081d53b0edd92d0f066d7a918217e
Thapson <thapsonknyirenda@gmail.com>
2025-01-02 10:53:20 +0200

    Learning models

---
+++ src/main/java/com/thapson/Customer.java
@@ -0,0 +1,13 @@
+package com.thapson;
+
+public class Customer {
+    private int id;
+    private int age;
+    private String name;
+
+    public Customer(int id, int age, String name){
+        this.id=id;
+        this.age=age;
+        this.name=name;
+    }
+}
