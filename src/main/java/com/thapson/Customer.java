311d6696bc25e05bbe45559cf2f44054c116da98
Thapson <thapsonknyirenda@gmail.com>
2025-01-02 18:05:52 +0200

    Working on docker and postgres

--- src/main/java/com/thapson/Customer.java
+++ src/main/java/com/thapson/Customer.java
@@ -1,5 +1,7 @@
 package com.thapson;
 
+import java.util.Objects;
+
 public class Customer {
     private int id;
     private int age;
@@ -9,5 +11,51 @@ public class Customer {
         this.id=id;
         this.age=age;
         this.name=name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && age == customer.age && Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
